7e79679b527eb63ef3a7d01fc3a0a6a47d016f7b
Kassandra Dominguez <kassdomlara02@gmail.com>
2023-06-22 15:52:41 -0600

    se  revuelve el ejercicio de live coding

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // Escribe tu código aquí.
+
+// creamos un arreglo
+const arreglo=["hola", "Java", "Javascript","programacion"];
+
+// funcion para determina la palabra mas larga
+
+function palabraMasLarga(){
+    let palabra="SI"; //definimos una palabra que sera  de referencia
+    let resultado=[];
+// este metodo evalua cada elemento del arreglo y ejecuta la funcion que definimos
+    arreglo.forEach(elemento => {
+        if(elemento.length>palabra.length){
+            resultado.push(elemento);
+
+        }
+    });
+
+return resultado
+
+   
+};
+
+// mostar la lista en el DOM
+
+nuevoarreglo=palabraMasLarga();
+console.log(nuevoarreglo);
+
+// traer el elemto del dom
+lista=document.getElementById("lista");
+
+for(let x=0; x<nuevoarreglo.length; x++){
+    dato=document.createElement("li");
+    dato.innerHTML=nuevoarreglo[x];
+    lista.appendChild(dato);
+};
+
+
+
+
+
+
